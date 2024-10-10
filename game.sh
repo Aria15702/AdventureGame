23394ed281549ac36a49a200c5fdc08ca30d90df
Aria <ASandhuCha@smail.pembinatrails.ca>
2024-10-10 13:36:37 -0500

    Your commit message here

--- game.sh
+++ game.sh
@@ -1,12 +1,14 @@
 echo "Welcome to the Ultimate Adventure Game!"
-echo "choose a location to explore: forest, castle, or cave."
+echo "Welcome to the Adventure Game!" echo "choose a location to explore: forest, castle, or cave."
 read location 
 if [ "$location" == "forest" ]; then 
-cat forest.txt
+ cat forest.txt
 elif [ "$location" == "castle" ]; then 
-cat castle.txt
+ cat castle.txt
 elif [ "$location" == "cave" ]; then 
-cat cave.txt
+ cat cave.txt
+echo "But wait..."
+cat monster.txt
 else
 echo "Invalid location"
 fi
